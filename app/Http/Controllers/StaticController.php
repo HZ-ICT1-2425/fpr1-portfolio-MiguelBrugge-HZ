82460c65d9ce2e0911dd3f0a693695c26c14a037
Miguel Brugge <brug0024@hz.nl>
2025-02-16 12:16:04 +0100

    Removed unwanted views, StaticController comments

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+
 class StaticController extends Controller
 {
+    /**
+     * @return View Page to load
+     * Home page
+     */
     public function home()
     {
         return view('home');
     }
 
+    /**
+     * @return View Page to load
+     * Dashboard page
+     */
     public function dashboard()
     {
         return view('dashboard');
     }
 
+    /**
+     * @return View Page to load
+     * Profile page
+     */
     public function profile()
     {
         return view('profile');
