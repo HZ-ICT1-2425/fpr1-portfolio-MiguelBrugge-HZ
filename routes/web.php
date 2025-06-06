2c0ee8359bc4755129e76858cd6a230ed9ca50b5
Miguel Brugge <brug0024@hz.nl>
2025-06-06 11:46:23 +0200

    added laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,37 @@ use App\Http\Controllers\StaticController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\FaqController;
 
-Route::get('/', [StaticController::class, 'home'])->name('home');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
 
-Route::get('profile', [StaticController::class, 'profile'])->name('profile');
+// Require login for ALL routes inside this group
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::get('dashboard', [StaticController::class, 'dashboard'])->name('dashboard');
+    // Static pages
+    Route::get('/', [StaticController::class, 'home'])->name('home');
+    Route::get('profile', [StaticController::class, 'profile'])->name('profile');
+    Route::get('dashboard', [StaticController::class, 'dashboard'])->name('dashboard');
 
-Route::get('faq', [FaqController::class, 'index'])->name('faq.index');
-Route::get('faq/create', [FaqController::class, 'create'])->name('faq.create');
-Route::post('faq', [FaqController::class, 'store'])->name('faq.store');
-Route::get('faq/{faq}/edit', [FaqController::class, 'edit'])->name('faq.edit');
-Route::put('faq/{faq}', [FaqController::class, 'update'])->name('faq.update');
-Route::delete('faq/{faq}', [FaqController::class, 'delete'])->name('faq.delete');
+    // FAQ routes
+    Route::get('faq', [FaqController::class, 'index'])->name('faq.index');
+    Route::get('faq/create', [FaqController::class, 'create'])->name('faq.create');
+    Route::post('faq', [FaqController::class, 'store'])->name('faq.store');
+    Route::get('faq/{faq}/edit', [FaqController::class, 'edit'])->name('faq.edit');
+    Route::put('faq/{faq}', [FaqController::class, 'update'])->name('faq.update');
+    Route::delete('faq/{faq}', [FaqController::class, 'delete'])->name('faq.delete');
 
-Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::get('posts/{slug}/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
+    // Post routes
+    Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::get('posts/{slug}/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
+});
+
+// Auth routes (login, register, etc.)
+require __DIR__.'/auth.php';
